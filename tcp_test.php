eac194608b5b71a2674ed479344e51e55a639384
jerry <1804823423@qq.com>
2017-06-13 10:55:48 +0800

    workerman TCP测试

---
+++ tcp_test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 18048
+ * Date: 2017/6/13
+ * Time: 10:53
+ */
+use Workerman\Worker;
+require_once __DIR__.'\Workerman\Autoloader.php';
+
+$tcp_worker = new Worker("tcp://0.0.0.0:2347");
+
+$tcp_worker->count=4;
+
+$tcp_worker->onMessage = function($connection,$data){
+    $connection->send('hello'.$data);
+};
+
+Worker::runAll();
